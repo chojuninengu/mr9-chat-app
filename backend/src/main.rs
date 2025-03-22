cbef68172ba2f13b5ed886e870c44f38e600490c
chojuninengu <chojuninengu@gmail.com>
2025-03-22 10:09:04 +0100

    removed server

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,15 +1,18 @@
 use axum::{routing::get, Router};
 use std::net::SocketAddr;
+use tokio::net::TcpListener;
 
 #[tokio::main]
 async fn main() {
-    let app = Router::new().route("/", get(|| async { "Hello, Chat App!" }));
+    let app = Router::new().route("/", get(|| async { "Hello, Mr-9 Chat App!" }));
 
     let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
-    println!("Server running on http://{}", addr);
 
-    axum::Server::bind(&addr)
-        .serve(app.into_make_service())
+    println!("🚀 Server running at http://{}", addr);
+
+    let listener = TcpListener::bind(addr).await.unwrap();
+
+    axum::serve(listener, app.into_make_service())
         .await
         .unwrap();
 }
